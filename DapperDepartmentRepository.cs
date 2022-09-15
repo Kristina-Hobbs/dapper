421da696401d54c65fc5bb3d95443aed9e15f475
Kristina Hobbs <kjcagle86@gmail.com>
2022-09-15 15:47:58 -0400

    setup dapper functionality

---
+++ DapperDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Application
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperDepartmentRepository(IDbConnection _connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<IDepartmentRepository>("SELECT * FROM departments;");
+        }
+    }
+}
+
